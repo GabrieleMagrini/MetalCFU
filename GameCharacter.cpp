9691e1855d1b45b04bdcef1a3918982553f1f9fa
emanuele <emanuele.nencioni@stud.unifi.it>
2019-05-23 18:01:11 +0200

    Added simple attribute and relative getter and setter for class "GameCharacter"

--- GameCharacter.cpp
+++ GameCharacter.cpp
@@ -3,3 +3,43 @@
 //
 
 #include "GameCharacter.h"
+
+int GameCharacter::getHp() const {
+    return HP;
+}
+
+void GameCharacter::setHp(int hp) {
+    HP = hp;
+}
+
+int GameCharacter::getPosX() const {
+    return posX;
+}
+
+void GameCharacter::setPosX(int posX) {
+    GameCharacter::posX = posX;
+}
+
+int GameCharacter::getPosY() const {
+    return posY;
+}
+
+void GameCharacter::setPosY(int posY) {
+    GameCharacter::posY = posY;
+}
+
+int GameCharacter::getStrenght() const {
+    return strenght;
+}
+
+void GameCharacter::setStrenght(int strenght) {
+    GameCharacter::strenght = strenght;
+}
+
+int GameCharacter::getMovementSpeed() const {
+    return movementSpeed;
+}
+
+void GameCharacter::setMovementSpeed(int movementSpeed) {
+    GameCharacter::movementSpeed = movementSpeed;
+}
--- GameCharacter.h
+++ GameCharacter.h
@@ -13,7 +13,34 @@ public:
     ~GameCharacter();
 
 private:
-    
+    int HP;
+    int posX;
+public:
+    int getHp() const;
+
+    void setHp(int hp);
+
+    int getPosX() const;
+
+    void setPosX(int posX);
+
+    int getPosY() const;
+
+    void setPosY(int posY);
+
+    int getStrenght() const;
+
+    void setStrenght(int strenght);
+
+    int getMovementSpeed() const;
+
+    void setMovementSpeed(int movementSpeed);
+
+private:
+    int posY;
+    int strenght;
+
+    int movementSpeed;
 
 };
 
