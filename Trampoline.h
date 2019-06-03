31f53965f0795394a3450c3a438c01f7253a10ae
emanuele <emanuele.nencioni@stud.unifi.it>
2019-06-03 13:30:11 +0200

    Created class "Trampoline"

---
+++ Trampoline.h
@@ -0,0 +1,22 @@
+//
+// Created by emanuele on 03/06/19.
+//
+
+#ifndef METALCFU_TRAMPOLINE_H
+#define METALCFU_TRAMPOLINE_H
+
+
+#include "Interactable.h"
+
+class Trampoline: public Interactable {
+public:
+    explicit Trampoline(int h,int r, int x, int y):height(h), Interactable(r,x,y){};
+
+    void effect() override;
+
+private:
+    int height;
+};
+
+
+#endif //METALCFU_TRAMPOLINE_H
