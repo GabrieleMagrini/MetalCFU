7e5fb457a1845a122aa9d8b32a093af7315e78aa
emanuele <emanuele.nencioni@stud.unifi.it>
2019-06-25 15:33:11 +0200

    Added in Directory Attachment new Class : "PowerBarrel"

---
+++ Attachment/PowerBarrel.cpp
@@ -0,0 +1,5 @@
+//
+// Created by emanuele on 25/06/19.
+//
+
+#include "PowerBarrel.h"
---
+++ Attachment/PowerBarrel.h
@@ -0,0 +1,14 @@
+//
+// Created by emanuele on 25/06/19.
+//
+
+#ifndef METALCFU_POWERBARREL_H
+#define METALCFU_POWERBARREL_H
+
+
+class PowerBarrel {
+
+};
+
+
+#endif //METALCFU_POWERBARREL_H
