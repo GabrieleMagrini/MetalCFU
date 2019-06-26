197242111695fb9f519052e04515d0886a47e9eb
emanuele <emanuele.nencioni@stud.unifi.it>
2019-06-26 10:27:49 +0200

    Create derived Class Enemy, mother class is "GameCharacter"

---
+++ Enemy.cpp
@@ -0,0 +1,5 @@
+//
+// Created by emanuele on 26/06/19.
+//
+
+#include "Enemy.h"
---
+++ Enemy.h
@@ -0,0 +1,15 @@
+//
+// Created by emanuele on 26/06/19.
+//
+
+#ifndef METALCFU_ENEMY_H
+#define METALCFU_ENEMY_H
+
+#include "GameCharacter.h"
+
+class Enemy: public GameCharacter {
+
+};
+
+
+#endif //METALCFU_ENEMY_H
