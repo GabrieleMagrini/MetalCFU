c4ba8c0369714f566aaec59a6797c6f1ef24196c
madmag <magro280899@gmail.com>
2019-07-15 18:48:06 +0200

    Added collision detection

--- Terrain.cpp
+++ Terrain.cpp
@@ -5,4 +5,13 @@
 #include "Terrain.h"
 
 
-int Terrain::checkCollision(GameCharacter *g, Ammo *a, Weapon *w, Interactable *i) {}
+int Terrain::checkCollision(GameCharacter *g, Ammo *a, Weapon *w, Interactable *i) {
+    if (g->getLocalBounds().intersects(this->getLocalBounds()))
+        g->setCollision(true);
+    if (a->getLocalBounds().intersects(this->getLocalBounds()))
+        a->setCollision(true);
+    if (w->getLocalBounds().intersects(this->getLocalBounds()))
+        w->setCollision(true);
+    if (i->getLocalBounds().intersects(this->getLocalBounds()))
+        i->setCollision(true);
+}
