e67764d191e40752277258ad7c6516f2269579e8
madmag <magro280899@gmail.com>
2019-07-15 22:28:00 +0200

    updated enemyFactory

--- Factory/EnemyFactory.cpp
+++ Factory/EnemyFactory.cpp
@@ -9,20 +9,24 @@
 #include "../SpecialAttack/BossAttack.h"
 
 std::unique_ptr<Enemy> EnemyFactory::createEnemy(EnemyType et) {
-    /* unique_ptr<Enemy> result;
+    unique_ptr<Enemy> result;
      WeaponFactory wf;
      switch(et){
          case EnemyType::Soldier:
-             result= unique_ptr<Enemy>(new Enemy(100, 0, 0, 10, (wf.createWeapon(WeaponType::M4)).get(), nullptr));
+             result = unique_ptr<Enemy>(
+                     new Enemy(100, 0, 0, 10, nullptr, (wf.createWeapon(WeaponType::M4)).get(), nullptr));
              break;
          case EnemyType::Tank:
-             result = unique_ptr<Enemy>(new Enemy(200, 0, 0, 10, (wf.createWeapon(WeaponType::M4)).get(), nullptr,new Charge(),0,0,5));
+             result = unique_ptr<Enemy>(
+                     new Enemy(200, 0, 0, 10, nullptr, (wf.createWeapon(WeaponType::M4)).get(), nullptr, 0, 0, 5));
              break;
          case EnemyType ::Kamikaze:
-             result = unique_ptr<Enemy>(new Enemy(75, 0, 0, 40, nullptr, nullptr,new Kamikaze(),0,0,15));
+             result = unique_ptr<Enemy>(new Enemy(75, 0, 0, 40, nullptr, nullptr, nullptr, 0, 0, 15));
              break;
          case EnemyType ::Boss:
-             result = unique_ptr<Enemy>(new Enemy(500, 0, 0, 10, (wf.createWeapon(WeaponType::AK_47)).get(), nullptr,new BossAttack(),0,0,10));
+             result = unique_ptr<Enemy>(
+                     new Enemy(500, 0, 0, 10, nullptr, (wf.createWeapon(WeaponType::AK_47)).get(), nullptr, 0, 0, 10));
      }
-     return result;*/
+    return result;
+
 }
