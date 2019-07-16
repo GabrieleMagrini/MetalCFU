cd435cbe538a1fbfae2763c4ea6991c56020ceae
madmag <magro280899@gmail.com>
2019-07-16 17:55:14 +0200

    updated checkCollision method

--- Terrain.cpp
+++ Terrain.cpp
@@ -3,23 +3,13 @@
 //
 
 #include "Terrain.h"
+#include "Collision.h"
 
 
 int Terrain::checkCollision(GameCharacter *g, Ammo *a, Weapon *w, Interactable *i) {
 
-    g->setCollision(true);
-
-    g->setCollision(false);
-    if (a->getLocalBounds().intersects(this->getLocalBounds()))
-        a->setCollision(true);
-    else
-        a->setCollision(false);
-    if (w->getLocalBounds().intersects(this->getLocalBounds()))
-        w->setCollision(true);
-    else
-        w->setCollision(false);
-    if (i->getLocalBounds().intersects(this->getLocalBounds()))
-        i->setCollision(true);
-    else
-        i->setCollision(false);
+    g->setCollision(Collision::PixelPerfectTest(*g, *this));
+    a->setCollision(Collision::PixelPerfectTest(*a, *this));
+    w->setCollision(Collision::PixelPerfectTest(*w, *this));
+    i->setCollision(Collision::PixelPerfectTest(*i, *this));
 }
