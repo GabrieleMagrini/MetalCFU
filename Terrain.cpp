4f253a74c8d790b993b2e20db61f3c157139c16a
madmag <magro280899@gmail.com>
2019-07-22 16:45:47 +0200

    corrected checkcollision return

--- Terrain.cpp
+++ Terrain.cpp
@@ -6,7 +6,7 @@
 #include "Collision.h"
 
 
-int Terrain::checkCollision(GameCharacter *g, Ammo *a, Weapon *w, Interactable *i) {
+void Terrain::checkCollision(GameCharacter *g, Ammo *a, Weapon *w, Interactable *i) {
 
     g->setCollision(Collision::PixelPerfectTest(*g, *this));
     a->setCollision(Collision::PixelPerfectTest(*a, *this));
