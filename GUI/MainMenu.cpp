add1b5afe9e2c6e61c0c225708313b867308441e
emanuele <emanuele.nencioni@stud.unifi.it>
2019-07-26 14:14:41 +0200

    in class MainMenu created better constructor, and modify it to make  betterlook menu

--- GUI/MainMenu.cpp
+++ GUI/MainMenu.cpp
@@ -11,34 +11,43 @@
  */
 MainMenu::MainMenu(std::unique_ptr<sf::RenderWindow> rw, std::string filename, sf::Font font) : renderWin(
         std::move(rw)),
-                                                                                                startButton(50, 100,
-                                                                                                            200, 50,
-                                                                                                            font,
-                                                                                                            "Start",
-                                                                                                            "/run/media/emanuele/Volume/Dati/Università/Progetto Programmazione/MetalCFU/GUI/texture/idleGreenButton.png",
-                                                                                                            "/run/media/emanuele/Volume/Dati/Università/Progetto Programmazione/MetalCFU/GUI/texture/hoverGreenButton.png",
-                                                                                                            "/run/media/emanuele/Volume/Dati/Università/Progetto Programmazione/MetalCFU/GUI/texture/pressedGreenButton.png"),
+                                                                                                startButton(
+                                                                                                        (*renderWin).getSize().x /
+                                                                                                        2.0f - 200, 100,
+                                                                                                        400, 100,
+                                                                                                        font,
+                                                                                                        "Start",
+                                                                                                        "/run/media/emanuele/Volume/Dati/Università/Progetto Programmazione/MetalCFU/GUI/texture/idleGreenButton.png",
+                                                                                                        "/run/media/emanuele/Volume/Dati/Università/Progetto Programmazione/MetalCFU/GUI/texture/hoverGreenButton.png",
+                                                                                                        "/run/media/emanuele/Volume/Dati/Università/Progetto Programmazione/MetalCFU/GUI/texture/pressedGreenButton.png"),
 
-                                                                                                optionButton(50, 100,
-                                                                                                             200, 50,
-                                                                                                             font,
-                                                                                                             "Start",
-                                                                                                             "/run/media/emanuele/Volume/Dati/Università/Progetto Programmazione/MetalCFU/GUI/texture/idleGreenButton.png",
-                                                                                                             "/run/media/emanuele/Volume/Dati/Università/Progetto Programmazione/MetalCFU/GUI/texture/hoverGreenButton.png",
-                                                                                                             "/run/media/emanuele/Volume/Dati/Università/Progetto Programmazione/MetalCFU/GUI/texture/pressedGreenButton.png"),
+                                                                                                optionButton(
+                                                                                                        (*renderWin).getSize().x /
+                                                                                                        2.0f - 200, 210,
+                                                                                                        400, 100,
+                                                                                                        font,
+                                                                                                        "Option",
+                                                                                                        "/run/media/emanuele/Volume/Dati/Università/Progetto Programmazione/MetalCFU/GUI/texture/idleGreenButton.png",
+                                                                                                        "/run/media/emanuele/Volume/Dati/Università/Progetto Programmazione/MetalCFU/GUI/texture/hoverGreenButton.png",
+                                                                                                        "/run/media/emanuele/Volume/Dati/Università/Progetto Programmazione/MetalCFU/GUI/texture/pressedGreenButton.png"),
                                                                                                 exitButton
-                                                                                                        (50, 100, 200,
-                                                                                                         50, font,
-                                                                                                         "Start",
+                                                                                                        ((*renderWin).getSize().x /
+                                                                                                         2.0f - 200,
+                                                                                                         320,
+                                                                                                         400, 100, font,
+                                                                                                         "Exit",
                                                                                                          "/run/media/emanuele/Volume/Dati/Università/Progetto Programmazione/MetalCFU/GUI/texture/idleGreenButton.png",
                                                                                                          "/run/media/emanuele/Volume/Dati/Università/Progetto Programmazione/MetalCFU/GUI/texture/hoverGreenButton.png",
                                                                                                          "/run/media/emanuele/Volume/Dati/Università/Progetto Programmazione/MetalCFU/GUI/texture/pressedGreenButton.png") {
     sf::Texture text;
     if (!text.loadFromFile(filename))
         text.setSrgb(true);
-
+    startButton.setCharacterSize(40);
+    optionButton.setCharacterSize(40);
+    exitButton.setCharacterSize(40);
     backGround.setTexture(text);
-
+    backGround.setOrigin((*renderWin).getSize().x / 2.0f, (*renderWin).getSize().y / 2.0f);
+    backGround.setScale(2.5, 2);
 
 }
 
@@ -53,9 +62,11 @@ void MainMenu::render() {
 }
 
 void MainMenu::update() {
-    auto mousePos = sf::Mouse::getPosition();
+    auto mousePos = sf::Mouse::getPosition(*renderWin);
     auto worldPos = (*renderWin).mapPixelToCoords(mousePos);
     startButton.update(worldPos);
     optionButton.update(worldPos);
     exitButton.update(worldPos);
+
+    (*renderWin).display();
 }
