0394ee51bb7cd2de766389f3846f7fd388a16846
emanuele <emanuele.nencioni@stud.unifi.it>
2019-07-26 11:26:29 +0200

    in class MainMenu created constructor, created method render, and created method update.

--- GUI/MainMenu.cpp
+++ GUI/MainMenu.cpp
@@ -3,3 +3,59 @@
 //
 
 #include "MainMenu.h"
+
+/***
+ *
+ * @param rw a pointer to a RenderWindow
+ * @param filename name of the file for the background
+ */
+MainMenu::MainMenu(std::unique_ptr<sf::RenderWindow> rw, std::string filename, sf::Font font) : renderWin(
+        std::move(rw)),
+                                                                                                startButton(50, 100,
+                                                                                                            200, 50,
+                                                                                                            font,
+                                                                                                            "Start",
+                                                                                                            "/run/media/emanuele/Volume/Dati/Università/Progetto Programmazione/MetalCFU/GUI/texture/idleGreenButton.png",
+                                                                                                            "/run/media/emanuele/Volume/Dati/Università/Progetto Programmazione/MetalCFU/GUI/texture/hoverGreenButton.png",
+                                                                                                            "/run/media/emanuele/Volume/Dati/Università/Progetto Programmazione/MetalCFU/GUI/texture/pressedGreenButton.png"),
+
+                                                                                                optionButton(50, 100,
+                                                                                                             200, 50,
+                                                                                                             font,
+                                                                                                             "Start",
+                                                                                                             "/run/media/emanuele/Volume/Dati/Università/Progetto Programmazione/MetalCFU/GUI/texture/idleGreenButton.png",
+                                                                                                             "/run/media/emanuele/Volume/Dati/Università/Progetto Programmazione/MetalCFU/GUI/texture/hoverGreenButton.png",
+                                                                                                             "/run/media/emanuele/Volume/Dati/Università/Progetto Programmazione/MetalCFU/GUI/texture/pressedGreenButton.png"),
+                                                                                                exitButton
+                                                                                                        (50, 100, 200,
+                                                                                                         50, font,
+                                                                                                         "Start",
+                                                                                                         "/run/media/emanuele/Volume/Dati/Università/Progetto Programmazione/MetalCFU/GUI/texture/idleGreenButton.png",
+                                                                                                         "/run/media/emanuele/Volume/Dati/Università/Progetto Programmazione/MetalCFU/GUI/texture/hoverGreenButton.png",
+                                                                                                         "/run/media/emanuele/Volume/Dati/Università/Progetto Programmazione/MetalCFU/GUI/texture/pressedGreenButton.png") {
+    sf::Texture text;
+    if (!text.loadFromFile(filename))
+        text.setSrgb(true);
+
+    backGround.setTexture(text);
+
+
+}
+
+/***
+ * function that draw all the button and background
+ */
+void MainMenu::render() {
+    (*renderWin).draw(backGround);
+    startButton.render(*renderWin);
+    optionButton.render(*renderWin);
+    exitButton.render(*renderWin);
+}
+
+void MainMenu::update() {
+    auto mousePos = sf::Mouse::getPosition();
+    auto worldPos = (*renderWin).mapPixelToCoords(mousePos);
+    startButton.update(worldPos);
+    optionButton.update(worldPos);
+    exitButton.update(worldPos);
+}
--- GUI/MainMenu.h
+++ GUI/MainMenu.h
@@ -5,9 +5,25 @@
 #ifndef METALCFU_MAINMENU_H
 #define METALCFU_MAINMENU_H
 
+#include <memory>
+
+#include "SFML/Graphics.hpp"
+#include "Button.h"
 
 class MainMenu {
+public:
+    MainMenu(std::unique_ptr<sf::RenderWindow> rw, std::string filename, sf::Font font);
 
+    void render();
+
+    void update();
+
+private:
+    sf::Sprite backGround;
+    std::unique_ptr<sf::RenderWindow> renderWin;
+    Button startButton;
+    Button optionButton;
+    Button exitButton;
 };
 
 
