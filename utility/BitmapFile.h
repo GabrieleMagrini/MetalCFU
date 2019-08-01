db81a51fab68b6d57294669e82b493dcdcc0c678
emanuele <emanuele.nencioni@stud.unifi.it>
2019-08-01 22:43:23 +0200

    in class BitmapFile, added attributes

--- utility/BitmapFile.h
+++ utility/BitmapFile.h
@@ -5,9 +5,16 @@
 #ifndef METALCFU_BITMAPFILE_H
 #define METALCFU_BITMAPFILE_H
 
+#include <iostream>
+#include <vector>
+#include <memory>
 
 class BitmapFile {
 
+private:
+    std::string filename;
+    std::unique_ptr<FILE> file;
+    std::vector<unsigned char> buffer;
 };
 
 
