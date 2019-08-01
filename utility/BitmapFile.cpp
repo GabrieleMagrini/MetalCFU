9784be9ac1cf141fb699a743945774626cff0b7b
emanuele <emanuele.nencioni@stud.unifi.it>
2019-08-01 22:33:32 +0200

    added directory utility. in this, added class BitmapFile

---
+++ utility/BitmapFile.cpp
@@ -0,0 +1,5 @@
+//
+// Created by emanuele on 01/08/19.
+//
+
+#include "BitmapFile.h"
---
+++ utility/BitmapFile.h
@@ -0,0 +1,14 @@
+//
+// Created by emanuele on 01/08/19.
+//
+
+#ifndef METALCFU_BITMAPFILE_H
+#define METALCFU_BITMAPFILE_H
+
+
+class BitmapFile {
+
+};
+
+
+#endif //METALCFU_BITMAPFILE_H
