83a6b8332102e82f29814ae194234df28cd8e6f1
emanuele <emanuele.nencioni@stud.unifi.it>
2019-08-07 16:03:04 +0200

    in class terrain, changing code for method check collision

--- Terrain.cpp
+++ Terrain.cpp
@@ -2,19 +2,38 @@
 // Created by madmag on 22/06/19.
 //
 
+#include <iostream>
 #include "Terrain.h"
 #include "Collision.h"
 
 
 void Terrain::checkCollision(GameCharacter *g) {
-    if (g->getSpeedX() >= 0)
-        g->setCollisionX(((this->getPosition().x) - (g->getPosition().x + g->getTexture()->getSize().x)) <= 0);
-    else if (g->getSpeedX() <= 0)
-        g->setCollisionX(((this->getPosition().x + this->getTexture()->getSize().x) - g->getPosition().x) >= 0);
-    if (g->getSpeedY() >= 0 && g->getPosition().x == this->getPosition().x)
-        g->setCollisionY(((this->getPosition().y) - (g->getPosition().y + g->getTexture()->getSize().y)) <= 0);
-    else if (g->getSpeedY() <= 0 && g->getPosition().x == this->getPosition().x)
-        g->setCollisionY(((this->getPosition().y + this->getTexture()->getSize().y) - g->getPosition().y) >= 0);
+    /* if (g->getSpeedX() >= 0)
+         g->setCollisionX(((this->getPosition().x) - (g->getPosition().x + g->getTexture()->getSize().x)) <= 0);
+     else if (g->getSpeedX() <= 0)
+         g->setCollisionX(((this->getPosition().x + this->getTexture() ->getSize().x) - g->getPosition().x) >= 0);
+     if (g->getSpeedY() >= 0 && g->getPosition().x == this->getPosition().x)
+         g->setCollisionY(((this->getPosition().y) - (g->getPosition().y + g->getTexture()->getSize().y)) <= 0);
+     else if (g->getSpeedY() <= 0 && g->getPosition().x == this->getPosition().x)
+         g->setCollisionY(((this->getPosition().y + this->getTexture()->getSize().y) - g->getPosition().y) >= 0);
+ */
+    if ((this->getLocalBounds().width + this->getPosition().x - g->getPosition().x) >= 0 &&
+        (this->getLocalBounds().width + this->getPosition().x - g->getPosition().x) <= this->getLocalBounds().width) {
+
+        if ((this->getPosition().y - g->getPosition().y) >= 0 &&
+            (this->getPosition().y - g->getPosition().y) <=
+            g->getLocalBounds().height) {
+            if (g->getSpeedY() != 0) {
+
+                g->setCollisionY(true);
+                g->setPosition(g->getPosition().x, this->getPosition().y - g->getLocalBounds().height);
+            } else if (g->getSpeedX() != 0)
+                g->setCollisionX(Collision::PixelPerfectTest(*this, *g));
+            else {
+                g->setCollisionY(false);
+            }
+        }
+    }
 
 }
 
@@ -22,4 +41,5 @@ int Terrain::setBonus(GameCharacter *g, int bonusSpeedMovement) {
     while ((g->getCollisionY()) && Collision::PixelPerfectTest(*g, *this))
         g->setSpeedX(g->getSpeedX() + bonusSpeedMovement);
 
+    return 0;
 }
