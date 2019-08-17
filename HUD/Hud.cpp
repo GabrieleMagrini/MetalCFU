965ca3a90c7fee6e3445f68b8877de66507b73f4
emanuele <emanuele.nencioni@stud.unifi.it>
2019-08-17 18:22:24 +0200

    Adding class Hud, in directory "HUD/", that will create the Hud of the player

---
+++ HUD/Hud.cpp
@@ -0,0 +1,61 @@
+//
+// Created by emanuele on 16/08/19.
+//
+
+#include "Hud.h"
+
+Hud::Hud(std::shared_ptr<sf::RenderWindow> renderWin, const sf::Font &font) : renderWin(std::move(renderWin)) {
+    weaponAmmo.setString("name    --/--");
+    weaponAmmo.setPosition(this->renderWin->getSize().x - weaponAmmo.getLocalBounds().width, 10);
+    weaponAmmo.setFillColor(sf::Color::Red);
+    weaponAmmo.setFont(font);
+    weaponAmmo.setCharacterSize(24);
+
+    lives.setString("3");
+    lives.setFont(font);
+    lives.setPosition(10, 5);
+    lives.setFillColor(sf::Color::Black);
+    lives.setCharacterSize(15);
+
+    lifeBorder.setSize(sf::Vector2f(300, 20));
+    lifeBorder.setOutlineColor(sf::Color::Black);
+    lifeBorder.setPosition(10, 10);
+
+    life.setSize(sf::Vector2f(298, 18));
+    life.setFillColor(sf::Color::Red);
+    life.setPosition(11, 21);
+}
+
+void Hud::render() {
+    renderWin->draw(weaponAmmo);
+    renderWin->draw(lives);
+    renderWin->draw(lifeBorder);
+    renderWin->draw(life);
+}
+
+void Hud::update(Player &p, sf::View &view) {
+
+    float top = view.getCenter().y - view.getSize().y / 2.0f;
+    float left = view.getCenter().x - view.getSize().x / 2.0f;
+
+    life.setSize(sf::Vector2f(p.getHp() * life.getSize().x / 100, life.getLocalBounds().height));
+    stringstream ss;
+    if (p.getWeapon()->getMaxAmmo().getQuantity() == -1) {
+        ss << p.getWeapon()->getName() << "   " << "--/--";
+    } else {
+        ss << p.getWeapon()->getName() << "   " << p.getWeapon()->getCurrentAmmo().getQuantity() << "/"
+           << p.getWeapon()->getMaxAmmo().getQuantity();
+    }
+    weaponAmmo.setString(ss.str());
+    ss.str("");
+
+    ss << "Lives: " << p.getLives();
+    lives.setString(ss.str());
+
+    lifeBorder.setPosition(left + 10, top + 20);
+    life.setPosition(left + 11, top + 21);
+    weaponAmmo.setPosition(view.getCenter().x - weaponAmmo.getLocalBounds().width / 2.0f, top + 10);
+    lives.setPosition(left + 20, top);
+
+
+}
---
+++ HUD/Hud.h
@@ -0,0 +1,32 @@
+//
+// Created by emanuele on 16/08/19.
+//
+
+#ifndef METALCFU_HUD_H
+#define METALCFU_HUD_H
+
+#include <memory>
+#include <sstream>
+
+#include "../Ammo.h"
+#include "../Player.h"
+
+class Hud {
+public:
+    explicit Hud(std::shared_ptr<sf::RenderWindow> renderWin, const sf::Font &font);
+
+    void render();
+
+    void update(Player &p, sf::View &view);
+
+private:
+    std::shared_ptr<sf::RenderWindow> renderWin;
+    sf::Text weaponAmmo;
+    sf::Text lives;
+    sf::RectangleShape life;
+    sf::RectangleShape lifeBorder;
+
+};
+
+
+#endif //METALCFU_HUD_H
