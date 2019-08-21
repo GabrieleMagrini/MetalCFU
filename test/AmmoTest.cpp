6b78915889378fa2ef5b497783a84384c5d355fb
emanuele <emanuele.nencioni@stud.unifi.it>
2019-08-21 21:24:08 +0200

    modify all AmmoTest

--- test/AmmoTest.cpp
+++ test/AmmoTest.cpp
@@ -7,21 +7,35 @@
 #include "../Ammo.h"
 
 TEST(Ammo, constructor) {
-    Ammo a{6};
-    ASSERT_EQ(a.getQuantity(), 6);
+    Ammo a{6, 4, false, false};
+    ASSERT_EQ(a.getDamage(), 6);
+    ASSERT_EQ(a.getRange(), 4);
+    ASSERT_EQ(a.getTerrainCollision(), false);
+    ASSERT_EQ(a.isIsShot(), false);
+    auto b = a;
+    ASSERT_EQ(b.getDamage(), 6);
+    ASSERT_EQ(b.getRange(), 4);
+    ASSERT_EQ(b.getTerrainCollision(), false);
+    ASSERT_EQ(b.isIsShot(), false);
 }
 
-TEST(Ammo, setQuatity) {
+TEST(Ammo, setDamage) {
     Ammo a;
-    ASSERT_EQ(a.getQuantity(), 1);
-    a.setQuantity(15);
-    ASSERT_EQ(a.getQuantity(), 15);
+    ASSERT_EQ(a.getDamage(), 1);
+    a.setDamage(15);
+    ASSERT_EQ(a.getDamage(), 15);
 }
 
 TEST(Ammo, equalOperator){
     Ammo a{3};
     Ammo a1;
     ASSERT_EQ(a==a1,false);
-    a1.setQuantity(3);
+    a1.setDamage(3);
     ASSERT_EQ(a==a1,true);
+}
+
+TEST(Ammo, setRange) {
+    Ammo a;
+    a.setRange(10);
+    ASSERT_EQ(a.getRange(), 10);
 }
