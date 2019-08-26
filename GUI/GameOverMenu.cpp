2dc6a51210af5b86e296ac3344e96f0dca83082a
emanuele <emanuele.nencioni@stud.unifi.it>
2019-08-26 21:34:51 +0200

    In directory GUI, added class GameOverMenu

---
+++ GUI/GameOverMenu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by emanuele on 26/08/19.
+//
+
+#include "GameOverMenu.h"
---
+++ GUI/GameOverMenu.h
@@ -0,0 +1,14 @@
+//
+// Created by emanuele on 26/08/19.
+//
+
+#ifndef METALCFU_GAMEOVERMENU_H
+#define METALCFU_GAMEOVERMENU_H
+
+
+class GameOverMenu {
+
+};
+
+
+#endif //METALCFU_GAMEOVERMENU_H
