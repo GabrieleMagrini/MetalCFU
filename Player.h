c8669348e3f71b1dec143ac7acd9cc164bd4a778
emanuele <emanuele.nencioni@stud.unifi.it>
2019-08-29 22:44:15 +0200

    In class Player, adding usable* in parameter

--- Player.h
+++ Player.h
@@ -11,12 +11,14 @@
 class Player: virtual public GameCharacter{
 public:
 
-    explicit Player(int l, Weapon *w, int hp = 100, int s = 20, int x = 10, int y = 10) : lives(l),
-                                                                                          GameCharacter(hp, x, y, s, w,
-                                                                                                        nullptr, 4, 4,
-                                                                                                        10, -10, 0, 4,
-                                                                                                        false,
-                                                                                                        "Sources/Pngs/player textures") {}
+    explicit Player(int l, Weapon *w, Usable *u = nullptr, int hp = 100, int s = 20, int x = 10, int y = 10) : lives(l),
+                                                                                                               GameCharacter(hp, x, y, s, w,
+                                                                                                                             u,
+                                                                                                                             4,
+                                                                                                                             4,
+                                                                                                                             10, -10, 0, 4,
+                                                                                                                             false,
+                                                                                                                             "Sources/Pngs/player textures") {}
     ~Player()override = default;
 
     void lootPickUp(Player *p);
