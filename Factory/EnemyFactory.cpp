415e04e943bf05df82d8d0c1f23bda64d86e0d25
emanuele <emanuele.nencioni@stud.unifi.it>
2019-09-01 19:22:46 +0200

    In class EnemyFactory, in method createEnemy, adjust constructor with {}.

--- Factory/EnemyFactory.cpp
+++ Factory/EnemyFactory.cpp
@@ -16,19 +16,19 @@ std::unique_ptr<Enemy> EnemyFactory::createEnemy(EnemyType et) {
      switch(et){
          case EnemyType::Soldier:
              result = unique_ptr<Enemy>(
-                     new Enemy(100, 0, 0, 10, nullptr, (wf.createWeapon(WeaponType::AK_47)).get(), nullptr, 1, 0, 4));
+                     new Enemy{100, 0, 0, 10, nullptr, (wf.createWeapon(WeaponType::AK_47)).get(), nullptr, 1, 0, 4});
              result->setTexture(*soldier);
              break;
          case EnemyType::Tank:
              result = unique_ptr<Enemy>(
-                     new Enemy(200, 0, 0, 10, nullptr, (wf.createWeapon(WeaponType::M4)).get(), nullptr, 1, 0, 2));
+                     new Enemy{200, 0, 0, 10, nullptr, (wf.createWeapon(WeaponType::M4)).get(), nullptr, 1, 0, 2});
              break;
          case EnemyType ::Kamikaze:
-             result = unique_ptr<Enemy>(new Enemy(75, 0, 0, 40, nullptr, nullptr, nullptr, 1, 0, 8));
+             result = unique_ptr<Enemy>(new Enemy{75, 0, 0, 40, nullptr, nullptr, nullptr, 1, 0, 8});
              break;
          case EnemyType ::Boss:
              result = unique_ptr<Enemy>(
-                     new Enemy(500, 0, 0, 10, nullptr, (wf.createWeapon(WeaponType::AK_47)).get(), nullptr, 0, 0, 3));
+                     new Enemy{500, 0, 0, 10, nullptr, (wf.createWeapon(WeaponType::AK_47)).get(), nullptr, 0, 0, 3});
      }
     return result;
 
