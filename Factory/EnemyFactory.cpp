3db81f0830b41a67b5dc4a6a7f752c9fd8b1b3e1
emanuele <emanuele.nencioni@stud.unifi.it>
2019-09-08 01:06:41 +0200

    In class EnemyFactory, update method to load texture.

--- Factory/EnemyFactory.cpp
+++ Factory/EnemyFactory.cpp
@@ -8,28 +8,29 @@
 
 std::unique_ptr<Enemy> EnemyFactory::createEnemy(EnemyType et) {
     unique_ptr<Enemy> result;
-     WeaponFactory wf;
-    auto soldier = new sf::Texture();
-    soldier->loadFromFile(
-            "Sources/Pngs/player textures/dx/1.png");
+    WeaponFactory wf;
 
-     switch(et){
-         case EnemyType::Soldier:
-             result = unique_ptr<Enemy>(
-                     new Enemy{100, 0, 0, 10, nullptr, (wf.createWeapon(WeaponType::AK_47)).get(), nullptr, 1, 0, 4});
-             result->setTexture(*soldier);
-             break;
-         case EnemyType::Tank:
-             result = unique_ptr<Enemy>(
-                     new Enemy{200, 0, 0, 10, nullptr, (wf.createWeapon(WeaponType::M4)).get(), nullptr, 1, 0, 2});
-             break;
-         case EnemyType ::Kamikaze:
-             result = unique_ptr<Enemy>(new Enemy{75, 0, 0, 40, nullptr, nullptr, nullptr, 1, 0, 8});
-             break;
-         case EnemyType ::Boss:
-             result = unique_ptr<Enemy>(
-                     new Enemy{500, 0, 0, 10, nullptr, (wf.createWeapon(WeaponType::AK_47)).get(), nullptr, 0, 0, 3});
-     }
+    switch (et) {
+        case EnemyType::Soldier:
+            result = unique_ptr<Enemy>(
+                    new Enemy{100, 0, 0, 10, nullptr, (wf.createWeapon(WeaponType::AK_47)).get(), nullptr,
+                              "Sources/Pngs/player textures/playerTexture.bmp", 1, 0, 4});
+            break;
+        case EnemyType::Tank:
+            result = unique_ptr<Enemy>(
+                    new Enemy{200, 0, 0, 10, nullptr, (wf.createWeapon(WeaponType::M4)).get(), nullptr,
+                              "Sources/Pngs/player textures/dx/1.png", 1, 0, 2});
+            break;
+        case EnemyType::Kamikaze:
+            result = unique_ptr<Enemy>(
+                    new Enemy{75, 0, 0, 40, nullptr, nullptr, nullptr, "Sources/Pngs/player textures/dx/1.png",
+                              1, 0, 8});
+            break;
+        case EnemyType::Boss:
+            result = unique_ptr<Enemy>(
+                    new Enemy{500, 0, 0, 10, nullptr, (wf.createWeapon(WeaponType::AK_47)).get(), nullptr,
+                              "Sources/Pngs/player textures/dx/1.png", 0, 0, 3});
+    }
     return result;
 
 }
