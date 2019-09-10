3959906f43121bfc9c4f5adbda67593d970703d8
emanuele <emanuele.nencioni@stud.unifi.it>
2019-09-10 21:04:06 +0200

    now player derived from Subject, adding new attribute "std::list<Observer>" override function of Subject

--- Player.cpp
+++ Player.cpp
@@ -4,15 +4,24 @@
 
 #include "Player.h"
 
-void Player::lootPickUp(Player *p) {
-
-
-}
-
 int Player::getLives() const {
     return lives;
 }
 
 void Player::setLives(int lives) {
     Player::lives = lives;
+}
+
+void Player::subscribe(Observer *o) {
+    observers.push_back(o);
+}
+
+void Player::unsubscribe(Observer *o) {
+    observers.remove(o);
+}
+
+void Player::notify() {
+    for (auto &obs: observers) {
+        obs->update();
+    }
 }
--- Player.h
+++ Player.h
@@ -5,10 +5,14 @@
 #ifndef METALCFU_PLAYER_H
 #define METALCFU_PLAYER_H
 
+#include <list>
+
 #include "GameCharacter.h"
 #include "Inventory.h"
+#include "Achievement/Subject.h"
+#include "Achievement/Observer.h"
 
-class Player: virtual public GameCharacter{
+class Player : virtual public GameCharacter, public Subject {
 public:
 
     explicit Player(int l, Weapon *w, Usable *u = nullptr, int hp = 100, int s = 20, int x = 10, int y = 10) : lives(l),
@@ -21,15 +25,21 @@ public:
                                                                                                                              "Sources/Pngs/player textures/playerTexture.bmp") {}
     ~Player()override = default;
 
-    void lootPickUp(Player *p);
 
     int getLives() const;
 
     void setLives(int lives);
 
-private:
+    void subscribe(Observer *o) override;
 
+    void unsubscribe(Observer *o) override;
+
+    void notify() override;
+
+private:
     int lives;
+
+    std::list<Observer *> observers;
 };
 
 
