151fdf114cb6e8b11d4df4b4156784f762fbdceb
madmag <magro280899@gmail.com>
2019-09-10 11:59:46 +0200

    Fixed test Collision + Attachment

--- test/CollisionTest.cpp
+++ test/CollisionTest.cpp
@@ -2,7 +2,7 @@
 // Created by madmag on 25/07/19.
 //
 
-/*
+
 #include "gtest/gtest.h"
 
 #include "../Terrain.h"
@@ -28,9 +28,16 @@ TEST(Collision, playerCollision) {
     player->setPosition(100, 100);
     player->setTexture(*block->getTexture());
 
-    block->checkCollision(player);
+    block->checkCollision(*player);
 
-    ASSERT_EQ(player->getCollisionX(), true);
+    ASSERT_EQ(player->isCollisionDown(), false);
+
+    ASSERT_EQ(player->isCollisionLeft(), false);
+
+    ASSERT_EQ(player->isCollisionUp(), false);
+
+    ASSERT_EQ(player->isCollisionRight(), true);
+
 
 
 }
@@ -51,9 +58,15 @@ TEST(Collision, playerNotCollided) {
     player->setPosition(0, 100);
     player->setTexture(*block->getTexture());
 
-    block->checkCollision(player);
+    block->checkCollision(*player);
 
-    ASSERT_EQ(player->getCollisionX(), false);
+    ASSERT_EQ(player->isCollisionDown(), false);
+
+    ASSERT_EQ(player->isCollisionLeft(), false);
+
+    ASSERT_EQ(player->isCollisionUp(), false);
+
+    ASSERT_EQ(player->isCollisionRight(), false);
 
 
 }
@@ -64,8 +77,8 @@ TEST(Collision, playerCollided) {
     auto block = f.createTerrain(TerrainType::Dirt);
     auto block2 = f.createTerrain(TerrainType::Dirt);
 
-    block->setPosition(100, 100);
-    block2->setPosition(0,100);
+    block->setPosition(60, 100);
+    block2->setPosition(50, 100);
 
     WeaponFactory w;
 
@@ -75,34 +88,16 @@ TEST(Collision, playerCollided) {
 
     player->setPosition(52, 100);
     player->setTexture(*block->getTexture());
-    
-    block->checkCollision(player);
 
-    ASSERT_EQ(player->getCollisionX(), true);
-
-}
+    block->checkCollision(*player);
+    block2->checkCollision(*player);
 
 
-TEST(Collision, collisionY) {
-    TerrainFactory f;
-    auto block = f.createTerrain(TerrainType::Dirt);
+    ASSERT_EQ(player->isCollisionLeft(), false);
 
-    block->setPosition(100, 100);
+    ASSERT_EQ(player->isCollisionUp(), false);
 
-    WeaponFactory w;
-
-    auto w1 = w.createWeapon(WeaponType::M4);
-
-    auto player = new Player(2, w1.get());
-
-    player->setPosition(0, 100);
-    player->setTexture(*block->getTexture());
-
-    block->checkCollision(player);
-
-    ASSERT_EQ(player->getCollisionY(), false);
+    ASSERT_EQ(player->isCollisionRight(), true);
 
 
 }
-
-*/
