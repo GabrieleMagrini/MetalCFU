360fd4ff9a028fc13541b7e7006c2a083db3a451
emanuele <emanuele.nencioni@stud.unifi.it>
2019-09-11 20:06:25 +0200

    In class DistanceObserver adjusting file

--- Achievement/DistanceObserver.cpp
+++ Achievement/DistanceObserver.cpp
@@ -20,6 +20,7 @@ void DistanceObserver::detach() {
 
 void DistanceObserver::update() {
     distanceTraveled = distanceTraveled + abs(subject->getPosition().x - startPosX);
+    startPosX = subject->getPosition().x;
     if (distanceTraveled >= distanceMax) {
         subject->unlock(AchievementType::DISTANCE);
         detach();
@@ -28,4 +29,5 @@ void DistanceObserver::update() {
 
 DistanceObserver::~DistanceObserver() {
     detach();
+    delete subject;
 }
