85a92a0beffda2cbdd1466cb47147343efe9b0f3
emanuele <emanuele.nencioni@stud.unifi.it>
2019-09-12 21:55:50 +0200

    In class Game, adding code for set position of the window

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@
 int main() {
 
     std::shared_ptr<RenderWindow> rw(new sf::RenderWindow(sf::VideoMode(1280, 720), "Metal CFU", sf::Style::Close));
+    rw->setPosition(sf::Vector2i(sf::VideoMode::getDesktopMode().width / 2 - rw->getSize().x / 2, 0));
     sf::Font font;
     font.loadFromFile("Sources/Raleway-Medium.ttf");
     Game g(rw, font);
